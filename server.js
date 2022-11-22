1d744f5305bd66bd2fb224d43694c69044a17f18
Omar Khalil <omareldagestany@outlook.com>
2022-11-22 00:26:51 -0500

    this try

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(express.static(`${__dirname}/client/build`));
 // importing routes
 const userRoutes = require("./routes/users");
 const postRoutes = require("./routes/posts");
