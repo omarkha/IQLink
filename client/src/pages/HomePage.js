c450a31507fb6ddefad4c8f9469b8fb75895ba91
Copyres <copyresearcher>
2023-06-07 01:56:14 -0400

    renamed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -15,17 +15,17 @@ const HomePage = (props) => {
         ) : (
           <header className="main-header">
             <div className="main-header-left-side">
-              <h1 className="display-1">IraqiLink</h1>
+              <h1 className="display-1">IraqiExpert</h1>
               <img src={iraq_map} alt="map of Iraq" />
             </div>
             <div className="main-header-info">
               <p>
-                اهلا وسهلا بك في موقع عراق لينك. حيث تستطيع ان تتواصل مع ناس من
+                اهلا وسهلا بك في موقع عراق اكسبرت. حيث تستطيع ان تتواصل مع ناس من
                 مختلف المهن. وان تنسق معهم على مشاريع شخصية ام مشاريع تفيد
                 المجتمع.
               </p>
               <p>
-                Hello and Welcome to IQLink. Where you can network with others
+                Hello and Welcome to IraqiExpert. Where you can network with others
                 from different professional background, and collaborate with
                 them on personal projects or ones that benefit soceity.
               </p>
